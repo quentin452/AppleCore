f86f977993f541ae80346af9d1aa77032f3c848a
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-09-10 15:08:42 +0200

    Fix the bug where it draws the food info hover NEI item even when NEI is closed (#17)
    * format
    
    * fix the bug where it shows the food values when nei is closed

--- src/main/java/squeek/applecore/client/TooltipOverlayHandler.java
+++ src/main/java/squeek/applecore/client/TooltipOverlayHandler.java
@@ -1,7 +1,14 @@
 package squeek.applecore.client;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.ObfuscationReflectionHelper;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.RenderTickEvent;
+import cpw.mods.fml.relauncher.ReflectionHelper;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiScreen;
@@ -19,125 +26,107 @@ import squeek.applecore.ModInfo;
 import squeek.applecore.api.AppleCoreAPI;
 import squeek.applecore.api.food.FoodValues;
 import squeek.applecore.helpers.KeyHelper;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Loader;
-import cpw.mods.fml.common.ObfuscationReflectionHelper;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.TickEvent;
-import cpw.mods.fml.common.gameevent.TickEvent.RenderTickEvent;
-import cpw.mods.fml.relauncher.ReflectionHelper;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 @SideOnly(Side.CLIENT)
-public class TooltipOverlayHandler
-{
-	private static ResourceLocation modIcons = new ResourceLocation(ModInfo.MODID_LOWER, "textures/icons.png");
+public class TooltipOverlayHandler {
 
-	public static void init()
-	{
-		FMLCommonHandler.instance().bus().register(new TooltipOverlayHandler());
-	}
+	private static final ResourceLocation modIcons = new ResourceLocation(ModInfo.MODID_LOWER, "textures/icons.png");
 	//private static final Field guiLeft = ReflectionHelper.findField(GuiContainer.class, ObfuscationReflectionHelper.remapFieldNames(GuiContainer.class.getName(), "guiLeft", "field_147003_i", "i"));
 	//private static final Field guiTop = ReflectionHelper.findField(GuiContainer.class, ObfuscationReflectionHelper.remapFieldNames(GuiContainer.class.getName(), "guiTop", "field_147009_r", "r"));
 	public static final Field theSlot = ReflectionHelper.findField(GuiContainer.class, ObfuscationReflectionHelper.remapFieldNames(GuiContainer.class.getName(), "theSlot", "field_147006_u", "u"));
 	private static Method getStackMouseOver = null;
+	private static Method isNEIHidden = null;
 	private static Field itemPanel = null;
 	private static boolean neiLoaded = false;
 	private static Class<?> foodJournalGui = null;
 	private static Field foodJournalHoveredStack = null;
 	public static int toolTipX, toolTipY, toolTipW, toolTipH;
-	static
-	{
-		try
-		{
+
+	static {
+		try {
 			neiLoaded = Loader.isModLoaded("NotEnoughItems");
-			if (neiLoaded)
-			{
+			if (neiLoaded) {
 				Class<?> LayoutManager = Class.forName("codechicken.nei.LayoutManager");
 				itemPanel = LayoutManager.getDeclaredField("itemPanel");
 				getStackMouseOver = Class.forName("codechicken.nei.ItemPanel").getDeclaredMethod("getStackMouseOver", int.class, int.class);
+				isNEIHidden = Class.forName("codechicken.nei.NEIClientConfig").getDeclaredMethod("isHidden");
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			AppleCore.Log.error("Unable to integrate the food values tooltip overlay with NEI: ");
 			e.printStackTrace();
 		}
-
-		try
-		{
-			if (Loader.isModLoaded("SpiceOfLife"))
-			{
+		try {
+			if (Loader.isModLoaded("SpiceOfLife")) {
 				foodJournalGui = ReflectionHelper.getClass(TooltipOverlayHandler.class.getClassLoader(), "squeek.spiceoflife.gui.GuiScreenFoodJournal");
 				foodJournalHoveredStack = ReflectionHelper.findField(foodJournalGui, "hoveredStack");
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			AppleCore.Log.error("Unable to integrate the food values tooltip overlay with The Spice of Life: ");
 			e.printStackTrace();
 		}
 	}
 
-	@SubscribeEvent
-	public void onRenderTick(RenderTickEvent event)
-	{
-		if (event.phase != TickEvent.Phase.END)
-			return;
+	public static void init() {
+		FMLCommonHandler.instance().bus().register(new TooltipOverlayHandler());
+	}
 
-		if ((ModConfig.SHOW_FOOD_VALUES_IN_TOOLTIP && KeyHelper.isShiftKeyDown()) || ModConfig.ALWAYS_SHOW_FOOD_VALUES_TOOLTIP)
-		{
+	@SubscribeEvent
+	public void onRenderTick(RenderTickEvent event) {
+
+		if (event.phase != TickEvent.Phase.END) {
+			return;
+		}
+
+		if ((ModConfig.SHOW_FOOD_VALUES_IN_TOOLTIP && KeyHelper.isShiftKeyDown()) || ModConfig.ALWAYS_SHOW_FOOD_VALUES_TOOLTIP) {
+
 			Minecraft mc = Minecraft.getMinecraft();
 			EntityPlayer player = mc.thePlayer;
 			GuiScreen curScreen = mc.currentScreen;
-
 			ScaledResolution scale = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
 
 			boolean isFoodJournalGui = foodJournalGui != null && foodJournalGui.isInstance(curScreen);
 			boolean isValidContainerGui = curScreen instanceof GuiContainer;
-			if (isValidContainerGui)
-			{
-				Gui gui = curScreen;
+			if (isValidContainerGui) {
 				int mouseX = Mouse.getX() * scale.getScaledWidth() / mc.displayWidth;
 				int mouseY = scale.getScaledHeight() - Mouse.getY() * scale.getScaledHeight() / mc.displayHeight;
 				ItemStack hoveredStack = null;
 
 				// get the hovered stack from the active container
-				try
-				{
+				try {
 					// try regular container
-					Slot hoveredSlot = (Slot) TooltipOverlayHandler.theSlot.get(gui);
+					Slot hoveredSlot = (Slot) TooltipOverlayHandler.theSlot.get(curScreen);
 
 					// get the stack
-					if (hoveredSlot != null)
+					if (hoveredSlot != null) {
 						hoveredStack = hoveredSlot.getStack();
+					}
 
 					// try NEI
-					if (hoveredStack == null && getStackMouseOver != null)
+					if (hoveredStack == null && isNEIHidden != null && !(boolean) isNEIHidden.invoke(null) && getStackMouseOver != null) {
 						hoveredStack = (ItemStack) (getStackMouseOver.invoke(itemPanel.get(null), mouseX, mouseY));
+					}
 
 					// try FoodJournal
-					if (hoveredStack == null && isFoodJournalGui)
-						hoveredStack = (ItemStack) foodJournalHoveredStack.get(gui);
-				}
-				catch (RuntimeException e)
-				{
+					if (hoveredStack == null && isFoodJournalGui) {
+						hoveredStack = (ItemStack) foodJournalHoveredStack.get(curScreen);
+					}
+				} catch (RuntimeException e) {
 					throw e;
-				}
-				catch (Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 
 				// if the hovered stack is a food and there is no item being dragged
-				if (player.inventory.getItemStack() == null && hoveredStack != null && AppleCoreAPI.accessor.isFood(hoveredStack))
-				{
+				if (player.inventory.getItemStack() == null && hoveredStack != null && AppleCoreAPI.accessor.isFood(hoveredStack)) {
 					FoodValues defaultFoodValues = FoodValues.get(hoveredStack);
 					FoodValues modifiedFoodValues = FoodValues.get(hoveredStack, player);
 
-					if (defaultFoodValues.equals(modifiedFoodValues) && defaultFoodValues.hunger == 0 && defaultFoodValues.saturationModifier == 0)
+					if (defaultFoodValues.equals(modifiedFoodValues) && defaultFoodValues.hunger == 0 && defaultFoodValues.saturationModifier == 0) {
 						return;
+					}
 
 					int biggestHunger = Math.max(defaultFoodValues.hunger, modifiedFoodValues.hunger);
 					float biggestSaturationIncrement = Math.max(defaultFoodValues.getSaturationIncrement(), modifiedFoodValues.getSaturationIncrement());
@@ -146,8 +135,9 @@ public class TooltipOverlayHandler
 					int saturationBarsNeeded = (int) Math.max(1, Math.ceil(Math.abs(biggestSaturationIncrement) / 2f));
 					boolean saturationOverflow = saturationBarsNeeded > 10;
 					String saturationText = saturationOverflow ? ((defaultFoodValues.saturationModifier < 0 ? -1 : 1) * saturationBarsNeeded) + "x " : null;
-					if (saturationOverflow)
+					if (saturationOverflow) {
 						saturationBarsNeeded = 1;
+					}
 
 					boolean needsCoordinateShift = !neiLoaded || isFoodJournalGui;
 					//int toolTipTopY = toolTipY;
@@ -163,8 +153,9 @@ public class TooltipOverlayHandler
 					int bottomY = topY + 20;
 
 					boolean wasLightingEnabled = GL11.glIsEnabled(GL11.GL_LIGHTING);
-					if (wasLightingEnabled)
+					if (wasLightingEnabled) {
 						GL11.glDisable(GL11.GL_LIGHTING);
+					}
 					GL11.glDisable(GL11.GL_DEPTH_TEST);
 
 					// bg
@@ -180,28 +171,29 @@ public class TooltipOverlayHandler
 
 					mc.getTextureManager().bindTexture(Gui.icons);
 
-					for (int i = 0; i < barsNeeded * 2; i += 2)
-					{
+					for (int i = 0; i < barsNeeded * 2; i += 2) {
 						x -= 9;
 
-						if (modifiedFoodValues.hunger < 0)
-							gui.drawTexturedModalRect(x, y, 34, 27, 9, 9);
-						else if (modifiedFoodValues.hunger > defaultFoodValues.hunger && defaultFoodValues.hunger <= i)
-							gui.drawTexturedModalRect(x, y, 133, 27, 9, 9);
-						else if (modifiedFoodValues.hunger > i + 1 || defaultFoodValues.hunger == modifiedFoodValues.hunger)
-							gui.drawTexturedModalRect(x, y, 16, 27, 9, 9);
-						else if (modifiedFoodValues.hunger == i + 1)
-							gui.drawTexturedModalRect(x, y, 124, 27, 9, 9);
-						else
-							gui.drawTexturedModalRect(x, y, 34, 27, 9, 9);
+						if (modifiedFoodValues.hunger < 0) {
+							curScreen.drawTexturedModalRect(x, y, 34, 27, 9, 9);
+						} else if (modifiedFoodValues.hunger > defaultFoodValues.hunger && defaultFoodValues.hunger <= i) {
+							curScreen.drawTexturedModalRect(x, y, 133, 27, 9, 9);
+						} else if (modifiedFoodValues.hunger > i + 1 || defaultFoodValues.hunger == modifiedFoodValues.hunger) {
+							curScreen.drawTexturedModalRect(x, y, 16, 27, 9, 9);
+						} else if (modifiedFoodValues.hunger == i + 1) {
+							curScreen.drawTexturedModalRect(x, y, 124, 27, 9, 9);
+						} else {
+							curScreen.drawTexturedModalRect(x, y, 34, 27, 9, 9);
+						}
 
 						GL11.glEnable(GL11.GL_BLEND);
 						GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-						gui.drawTexturedModalRect(x, y, defaultFoodValues.hunger - 1 == i ? 115 : 106, 27, 9, 9);
+						curScreen.drawTexturedModalRect(x, y, defaultFoodValues.hunger - 1 == i ? 115 : 106, 27, 9, 9);
 						GL11.glDisable(GL11.GL_BLEND);
 
-						if (modifiedFoodValues.hunger > i)
-							gui.drawTexturedModalRect(x, y, modifiedFoodValues.hunger - 1 == i ? 61 : 52, 27, 9, 9);
+						if (modifiedFoodValues.hunger > i) {
+							curScreen.drawTexturedModalRect(x, y, modifiedFoodValues.hunger - 1 == i ? 61 : 52, 27, 9, 9);
+						}
 					}
 
 					y += 11;
@@ -212,37 +204,36 @@ public class TooltipOverlayHandler
 					GL11.glPushMatrix();
 					GL11.glScalef(0.75F, 0.75F, 0.75F);
 					GL11.glColor4f(1f, 1f, 1f, .5f);
-					for (int i = 0; i < saturationBarsNeeded * 2; i += 2)
-					{
+					for (int i = 0; i < saturationBarsNeeded * 2; i += 2) {
 						float effectiveSaturationOfBar = (absModifiedSaturationIncrement - i) / 2f;
 
 						x -= 6;
 
 						boolean shouldBeFaded = absModifiedSaturationIncrement <= i;
-						if (shouldBeFaded)
-						{
+						if (shouldBeFaded) {
 							GL11.glEnable(GL11.GL_BLEND);
 							GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 						}
 
 						mc.getTextureManager().bindTexture(Gui.icons);
-						gui.drawTexturedModalRect(x * 4 / 3, y * 4 / 3, 16, 27, 9, 9);
+						curScreen.drawTexturedModalRect(x * 4 / 3, y * 4 / 3, 16, 27, 9, 9);
 
 						mc.getTextureManager().bindTexture(modIcons);
-						gui.drawTexturedModalRect(x * 4 / 3, y * 4 / 3, effectiveSaturationOfBar >= 1 ? 27 : effectiveSaturationOfBar > 0.5 ? 18 : effectiveSaturationOfBar > 0.25 ? 9 : effectiveSaturationOfBar > 0 ? 0 : 36, modifiedSaturationIncrement >= 0 ? 0 : 9, 9, 9);
+						curScreen.drawTexturedModalRect(x * 4 / 3, y * 4 / 3, effectiveSaturationOfBar >= 1 ? 27 : effectiveSaturationOfBar > 0.5 ? 18 : effectiveSaturationOfBar > 0.25 ? 9 : effectiveSaturationOfBar > 0 ? 0 : 36, modifiedSaturationIncrement >= 0 ? 0 : 9, 9, 9);
 
-						if (shouldBeFaded)
+						if (shouldBeFaded) {
 							GL11.glDisable(GL11.GL_BLEND);
+						}
 					}
-					if (saturationText != null)
-					{
+					if (saturationText != null) {
 						mc.fontRenderer.drawStringWithShadow(saturationText, x * 4 / 3 - mc.fontRenderer.getStringWidth(saturationText) + 2, y * 4 / 3 + 1, 0xFFFF0000);
 					}
 					GL11.glPopMatrix();
 
 					GL11.glEnable(GL11.GL_DEPTH_TEST);
-					if (wasLightingEnabled)
+					if (wasLightingEnabled) {
 						GL11.glEnable(GL11.GL_LIGHTING);
+					}
 					GL11.glColor4f(1f, 1f, 1f, 1f);
 				}
 			}
