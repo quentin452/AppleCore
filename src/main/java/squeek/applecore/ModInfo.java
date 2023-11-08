15b4d884113556852819adc6ed850e28408cb9f6
glowredman <35727266+glowredman@users.noreply.github.com>
2023-11-08 15:14:19 +0100

    Update buildscript

--- src/main/java/squeek/applecore/ModInfo.java
+++ src/main/java/squeek/applecore/ModInfo.java
@@ -6,8 +6,8 @@ public final class ModInfo {
 
     public static final String MODID = "AppleCore";
     public static final String MODAPI = "AppleCoreAPI";
-    public static final String VERSION = "GRADLETOKEN_VERSION";
-    public static final String APIVERSION = "GRADLETOKEN_VERSION";
+    public static final String VERSION = Tags.VERSION;
+    public static final String APIVERSION = Tags.VERSION;
     public static final String MODID_LOWER = ModInfo.MODID.toLowerCase(Locale.ROOT);
     public static final String GUI_FACTORY_CLASS = "squeek.applecore.client.gui.GuiFactory";
 }
